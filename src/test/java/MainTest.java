65b8e3798110c81d16dd18a8ebc7b0d649dd2575
Vlad <ystimenko_vlad@mail.ru>
2023-06-29 12:27:05 +0300

    Initial commit

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,31 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Cookie;
+
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+
+
+public class MainTest {
+    @BeforeEach
+    public void setUp() {
+        Configuration.baseUrl = "https://aptekaeconom.com/";
+        Cookie region = new Cookie("current_region", "119202");
+        open("/");
+        Selenide.webdriver().driver().getWebDriver().manage().addCookie(region);
+        refresh();
+        $(".region_wrapper .confirm_region").shouldNotBe(Condition.visible);
+    }
+
+    @Test
+    @DisplayName("Выбор любой подкатегории")
+    public void selectAnySubcategory() {
+        $(".inline-block .callback-block").click();
+        $("[name='CALLBACK']").shouldBe(Condition.visible);
+    }
+}
